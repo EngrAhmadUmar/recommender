962113b57adf39e13376c6fb9bc4727f43a6e571
Ahmad Ahmed <zcaptain2002@gmail.com>
2022-04-10 19:51:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openpyxl
 import xlrd
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMean
 from sklearn.linear_model import LinearRegression
 
 
@@ -35,7 +35,7 @@ def main():
     global dataframe
     if uploaded_file:
         df = pd.read_excel(uploaded_file)
-        dataframe = df
+        playlist_test = df
         # st.dataframe(df)
         # st.table(df)
 
@@ -45,19 +45,55 @@ def main():
     #
     # # Display Books
     if st.button("Predict"):
-      arr = dataframe.columns
+        def meanVectors(playlist):
+            vec = []
+            for song_id in playlist:
+                try:
+                    vec.append(model.wv[song_id])
+                except KeyError:
+                    continue
+            return np.mean(vec, axis=0)
+        def similarSongsByVector(vec, n = 10, by_name = True):
+            # extract most similar songs for the input vector
+            similar_songs = model.wv.similar_by_vector(vec, topn = n)
 
-      for i in arr:
-          notnull = dataframe[i][dataframe[i].notnull()]
-          min = notnull.min()
-          dataframe[i].replace(np.nan, min, inplace=True)
+            # extract name and similarity score of the similar products
+            if by_name:
+                similar_songs = [(songs.loc[int(song_id), "artist - title"], sim)
+                                      for song_id, sim in similar_songs]
 
-      scaler = StandardScaler()
-      scaler.fit(dataframe)
-      featureshost = scaler.transform(dataframe)
-      prediction = model.predict(featureshost)
+            return similar_songs
+        
+        playlist_vec = list(map(meanVectors, playlist_test))
+        def print_recommended_songs(idx, n):
+            print("============================")
+            print("SONGS PLAYLIST")
+            print("============================")
+            for song_id in playlist_test[idx]:
+                song_id = int(song_id)
+                print(songs.loc[song_id, "artist - title"])
+            print()
+            print("============================")
+            print(f"TOP {n} RECOMMENDED SONGS")
+            print("============================")
+            for song, sim in similarSongsByVector(playlist_vec[idx], n):
+                print(f"[Similarity: {sim:.3f}] {song}")
+            print("============================")
+            
+         
+#       arr = dataframe.columns
 
-      result = prediction
+#       for i in arr:
+#           notnull = dataframe[i][dataframe[i].notnull()]
+#           min = notnull.min()
+#           dataframe[i].replace(np.nan, min, inplace=True)
+
+#       scaler = StandardScaler()
+#       scaler.fit(dataframe)
+#       featureshost = scaler.transform(dataframe)
+#       prediction = model.predict(featureshost)
+
+      result = print_recommended_songs(idx = 305, n = 20)
       st.write(result)
 
 
